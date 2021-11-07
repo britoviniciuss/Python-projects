537894d3136b4a95bf6d9356e7e062a2fec91c44
brito <vinib.games@outlook.com>
2021-11-07 14:31:32 -0300

    Finished

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Python-projects
  Early python projects to train the concepts
 
-#########################################################
+###############################################################
 
-
-
-Some small python projects to train some basic concepts.
-
+Initial concepts were used to create some small python projects.
 
 1 - Dice simulator.
 
 2 - Choose a random number.
+
+3 - It's up to you.
+
+4 - Adventure game.
---
+++ adventure_game.py
@@ -0,0 +1,66 @@
+#Adventure game
+#O objetivo desse projeto é um jogo a onde terá varios finais de acordo com as respostas dada.
+
+#Estou numa guerra entre duas nações e temos dois lados: LadoA e LadoB. Somente um lado irá vencer. Tomar decisioes corretas para chegar a vitoria
+#do LadoA.
+import PySimpleGUI as sg
+
+
+class Jogo_aventura:
+    def __init__(self):
+        self.pergunta1 = 'Você nasceu no norte ou no sul?   (N/S)' #norte - lado a e sul - lado b.
+        self.pergunta2 = 'Você prefere a espada ou escudo? (espada/escudo)' #espada - ladoA e escudo LadoB
+        self.pergunta3 = 'Qual é sua especialidade ?  (frente/tatico)' #Linha de frente - LadoA, taitoc ladoB
+        self.finalHistoria1 = 'Você sera um heroi linha de frente'
+        self.finalHistoria2 = 'Você sera um heroi protegendo todas as nossas tropas'
+        self.finalHistoria3 = 'Você ira se sacrificar na batalha! '
+        self.finalHistoria4 = 'Você não é capaz de lutar essa batalha!'
+
+
+
+    def Iniciar(self):
+        #Layout
+        layout = [
+            [sg.Text(" ### Jogo de aventura ### ", size=(20, 0))],
+            [sg.Output(size=(45,0))],
+            [sg.Input(size=(30,0), key='escolha')],
+            [sg.Button('Iniciar'),sg.Button('Responder')],
+        ]
+        #Janela
+        self.janela = sg.Window('Jogo de Aventura!', layout=layout)
+
+        while True:
+            # Ler dados
+
+            self.LerValores()
+
+            # Fazer algo
+            if self.eventos == 'Iniciar':
+                print(self.pergunta1)
+                self.LerValores()
+
+                if self.valores['escolha'] == 'N':
+                    print(self.pergunta2)
+                    self.LerValores()
+                    if self.valores['escolha'] == 'espada':
+                        print(self.finalHistoria1)
+                    if self.valores['escolha'] == 'escudo':
+                        print(self.finalHistoria2)
+
+                if self.valores['escolha'] == 'S':
+
+                    print(self.pergunta3)
+                    self.LerValores()
+
+                    if self.valores['escolha'] == 'frente':
+                        print(self.finalHistoria3)
+
+                    if self.valores['escolha'] == 'tatico':
+                        print(self.finalHistoria4)
+
+    def LerValores(self):
+        self.eventos, self.valores = self.janela.Read()
+
+
+jogo = Jogo_aventura()
+jogo.Iniciar()
